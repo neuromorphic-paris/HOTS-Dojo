ebd20dead6c70f599c788d0bb2f31e854b36f1cb
marcorax <marcorax93@gmail.com>
2019-02-26 10:41:14 +0100

    Added few changes to compute the remaining compressive HOTS

--- Results_Comparison.py
+++ Results_Comparison.py
@@ -2,7 +2,7 @@ import pickle
 import matplotlib.pyplot as plt
 from matplotlib.ticker import PercentFormatter
 #%% Load Results
-res_file_name='1_L_Original_HOTS_Results_2019-02-20_15:14:37.654784.pkl'
+res_file_name='1_L_My_HOTS_Results_2019-02-20_15:14:23.008572.pkl'
 result_folder = "Results/Cards/"
 with open(result_folder+res_file_name, 'rb') as f:
        [mean,var,bench_results]= pickle.load(f)
